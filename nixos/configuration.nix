cf9997a1af56c94be1a2e16e9a1d56693c519cd2
Cvtq <62787854+Cvtq@users.noreply.github.com>
2024-11-26 14:02:02 +0300

    Fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,7 +15,21 @@
 
   i18n.defaultLocale = "en_US.UTF-8"; # Select internationalisation properties.
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
+    nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
+      substituters = [
+        #"http://binarycache.example.com"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        #"binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+
 
   system.stateVersion = "23.05"; # Don't change it bro
 }
