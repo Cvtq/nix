419f4f8b15c53093088a354e0ae1dec2468bfee3
Cvtq <62787854+Cvtq@users.noreply.github.com>
2024-11-27 14:11:43 +0300

    Fix

--- home-manager/modules/wms/sway.nix
+++ home-manager/modules/wms/sway.nix
@@ -1,12 +1,14 @@
 {  wayland.windowManager.sway = {
     enable = true;
     config = rec {
-      modifier = "Mod1";
+      modifier = "Mod4";
       # Use alacritty as default terminal
       terminal = "alacritty";
       startup = [
-        # Launch Firefox on start
-        {command = "alacritty";}
+        # Launch on start
+        {
+            #command = "alacritty";
+        }
       ];
     };
   };
