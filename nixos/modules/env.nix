910a7aeb4cafd766f08a76139896c86d8181d6d9
Cvtq <62787854+Cvtq@users.noreply.github.com>
2024-11-27 10:08:42 +0300

    Fix

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -5,4 +5,12 @@
     QT_QPA_PLATFORMTHEME = "qt5ct";
     GSETTINGS_BACKEND = "keyfile";
   };
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
 }
+
